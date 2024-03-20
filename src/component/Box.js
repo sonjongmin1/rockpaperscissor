c929ae9657ad839fb682a09cac06c351071d90d2
jong_min <whdals0525@gmail.com>
2024-03-20 19:37:27 +0900

    four-rock-paper-scissors

--- src/component/Box.js
+++ src/component/Box.js
@@ -21,7 +21,7 @@ const Box = (props) => {
     <div className={`box ${result}`}>
       <h1>{props.title}</h1>
       <h2 data-testid="item-name">{props.item && props.item.name}</h2>
-      <img className="item-img" src={props.item && props.item.img} />
+      <img className="item-img" src={props.item && props.item.img} alt="" />
       <h2>{result}</h2>
     </div>
   );
